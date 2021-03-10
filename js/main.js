b0514c86a2be4e3d68a6330bef5be694aa5f42b2
kasskoo <kasskoo@outlook.fr>
2021-03-10 15:47:42 +0000

    modification scss

--- js/main.js
+++ js/main.js
@@ -8,8 +8,11 @@ const photo = document.querySelector(".photo");
 if (window.innerWidth > 1000) {
     wrapper.addEventListener("mousemove", () => {
         let x = (event.clientX - (window.innerWidth/2));
+
         bigCircle.style.transform = `translate3D(${x/50}px, 0, 0)`;
+
         smallCircle.style.transform = `translate3D(${-x/8.5}px, 0, 0)`;
+        
         photo.style.transform = `translate3D(${-x/35}px, 0, 0)`;
     });
 }
